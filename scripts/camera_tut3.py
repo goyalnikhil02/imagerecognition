0534ed8bc5254cf1a02d3f7970f60a37f00f5f8f
Nikhil GOYAL <G521784@mph.morpho.com>
2018-09-28 22:25:02 +0530

    Capture the Snap from Camera

---
+++ scripts/camera_tut3.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+cam = cv2.VideoCapture(0)
+#print(cam.get(cv2.CAP_PROP_FRAME_WIDTH ))
+
+tf, frame = cam.read()
+cv2.imshow("Single Frame",frame)
+
+#capture the frmae and save as image
+#cv2.imwrite("grey_camera.jpg", frame)
+
+cv2.waitKey(10)
+cam.release()
+cv2.destroyAllWindows()
