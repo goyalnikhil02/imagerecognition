c89d52f2e7b322736989e071b009f7ac40387d1e
Nikhil GOYAL <G521784@mph.morpho.com>
2018-09-28 22:22:41 +0530

    Capture the Image from Camera

--- scripts/image_tut1.py
+++ scripts/image_tut1.py
@@ -4,7 +4,9 @@ import numpy as np
 img1 = cv2.imread('C:\\Users\\g521784\\Desktop\\ImageRecognistation\\images\\nikhil.jpeg')
 
 print (img1)
-
-grayA = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-cv2.imshow('firstimg',grayA)
-cv2.imwrite("grey_this.jpg", grayA)
+if img1 is None:
+    print('No object')
+else:
+    grayA = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('firstimg',grayA)
+    cv2.imwrite("grey_this.jpg", grayA)
